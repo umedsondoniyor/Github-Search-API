e99760510ba371442a88e0669f49e825a786ea9b
Umed <umedzhon@dama.dev>
2020-01-05 16:41:32 +0300

    profiles and repos are done

---
+++ githubfinder/github.js
@@ -0,0 +1,23 @@
+class Github {
+  constructor() {
+     // property 
+     this.client_id = '82575c014d8dc01681ef';
+     this.client_secret = '1b48f05d369bdb3dcb0334ae35cc31265d58ab28';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
